5ba9782d4823b57ba6cd2243c772a51d39309d14
hrid620 <hridishere@gmail.com>
2022-08-13 12:22:40 +0600

    prime number

---
+++ primenumber.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int flag=0;
+
+    for(int i=2; i<=n; i++)
+    {
+        int flag=1;
+
+        for(int j=2; j<=i/2; j++)
+        {
+            if(i%j==0)
+            {
+                flag=0;
+                break;
+            }
+        }
+        if(flag==1)
+        {
+            cout<<i<<" " ;
+        }
+    }
+}
